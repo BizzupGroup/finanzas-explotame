e695c1998d0606d82db821c6011903c5c9211a42
BizzupGroup <BizzupGroup@gmail.com>
2026-02-19 09:23:34 -0400

    links

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,13 +16,15 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://finanzas-explotame-2r695olh3-saul435s-projects.vercel.app",
-    "https://finanzas-explotame.vercel.app"
+    "https://finanzas-explotame.vercel.app",
+    "https://finanzas-explotame-mgfxfih7s-bizzups-projects.vercel.app",
+    "https://finanzas-explotame-git-main-bizzups-projects.vercel.app"
 
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
