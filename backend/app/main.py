5c9a4b4885f292074ec3389fd9a848a571c4af7b
BizzupGroup <BizzupGroup@gmail.com>
2026-02-19 09:09:30 -0400

    *

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://finanzas-explotame-git-main-bizzups-projects.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
